62f9e301061dcae1245dabba0126e2af065c4add
sinankarayaman <34620815+sinankarayaman@users.noreply.github.com>
2017-12-25 00:14:10 +0300

    x

---
+++ Fonksiyonlar.php
@@ -0,0 +1,43 @@
+<?php
+
+function MesajOku($SmsApiKey, $ID){
+    global $SmsApiKey;
+
+    $Baglanti = 'http://sms-activate.ru/stubs/handler_api.php?api_key='.$SmsApiKey.'&action=getStatus&status=1&id='.$ID.'&forward=0';
+    $Kaynak = file_get_contents($Baglanti);
+    if(preg_match('@STATUS_OK@', $Kaynak)){
+
+        $Sms = trim(str_replace('STATUS_OK:', '', $Kaynak));
+        return array('Durum' => 1, 'Sms' => $Sms);
+
+    }else{
+
+        return array('Durum' => 0);
+        
+    }
+
+}
+
+function YeniNumaraVer(){
+    global $SmsApiKey;
+
+    $Servis = 'go';
+    //$Servis = 'ot';
+    $Baglanti = 'http://sms-activate.ru/stubs/handler_api.php?api_key='.$SmsApiKey.'&action=getNumber&service='.$Servis.'&forward=0&ref=sinankarayaman';
+    $Kaynak = file_get_contents($Baglanti);
+    if(preg_match('@ACCESS_NUMBER@', $Kaynak)){
+
+        $NumaraBilgisi = explode(':', str_replace('ACCESS_NUMBER:', '', $Kaynak));
+
+        $ID = $NumaraBilgisi[0];
+        $Numara = $NumaraBilgisi[1];
+        return array('Durum' => 1, 'ID' => $ID, 'Numara' => $Numara, 'Site' => 'SmsActivate');
+
+    }else{
+
+        return array('Durum' => 0);
+
+    }
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+
+error_reporting(0);
+$SmsApiKey = 'REPLACE_API_KEY';
+
+include('Fonksiyonlar.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Numara</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+	<div id="Genel">
+		
+		<center><a href="index.php?Neresi=NumaraVer" class="numaraVer">Numara Ver</a></center>
+
+	<?php
+
+		if(isset($_GET['Neresi']) && $_GET['Neresi']=='YeniMesaj'){
+
+		    $TelefonNumaraID = $_GET['NumaraID'];
+		    $Baglanti = 'http://sms-activate.ru/stubs/handler_api.php?api_key='.$SmsApiKey.'&action=setStatus&status=3&id='.$TelefonNumaraID.'&forward=0'; // Tekrar Başka Sms Kontrolü
+		    $Kaynak = file_get_contents($Baglanti);
+
+		    if(preg_match('@ACCESS_RETRY_GET@', $Kaynak)){
+
+		?>
+
+			<div class="numaralar" style="margin-top: 25px;">Numara Yenilendi</div>
+
+		<?php
+
+		    }else{
+
+		?>
+
+			<div class="numaralar" style="margin-top: 25px;">Numara YENİLENEMEDİ</div>
+
+		<?php
+		    }
+
+		}
+
+		if(isset($_GET['Neresi']) && $_GET['Neresi']=='MesajOku'){
+
+		    $TelefonNumaraID = $_GET['NumaraID'];
+		    $MesajOku = MesajOku($SmsApiKey, $TelefonNumaraID);
+		    if($MesajOku['Durum']){
+
+		        $Sms = $MesajOku['Sms'];
+		        $Kod = preg_match('@([0-9]+)@', $Sms, $Kod)?end($Kod):false;
+		        if($Kod){
+
+		?>
+
+			<div class="numaralar" style="margin-top: 25px;">Mesaj: <?=$Kod?></div>
+
+		<?php
+
+		        }else{
+
+		?>
+
+			<div class="numaralar" style="margin-top: 25px;">Mesaj Bulunamadı</div>
+
+		<?php
+
+		        }
+
+		    }else{
+
+		?>
+
+			<div class="numaralar" style="margin-top: 25px;">Mesaj Bulunamadı</div>
+
+		<?php
+
+		    }
+
+		}
+
+		if(isset($_GET['Neresi']) && $_GET['Neresi']=='NumaraVer'){
+
+		    $YeniNumaraVer = YeniNumaraVer($SmsApiKey);
+		    $TelefonNumarasi = $YeniNumaraVer['Numara'];
+		    $TelefonNumaraID = $YeniNumaraVer['ID'];
+	?>
+
+			<div class="numaralar" style="margin-top: 25px;">Telefon Numarası: <span><?=$TelefonNumarasi?></span></div>
+			<div class="numaralar" style="margin-bottom: 25px;">Telefon Numarası ID: <span><?=$TelefonNumaraID?></span></div>
+
+		    <center><a href="index.php?Neresi=YeniMesaj&NumaraID=<?=$TelefonNumaraID?>" target="_blank" class="numaraVer">Numarayı Yenile</a></center>
+
+		    <center><a href="index.php?Neresi=MesajOku&NumaraID=<?=$TelefonNumaraID?>" target="_blank" class="numaraVer">Mesaj Kontrol</a></center>
+
+	<?php
+
+		}
+
+	?>
+	</div>
+
+</body>
+</html>
